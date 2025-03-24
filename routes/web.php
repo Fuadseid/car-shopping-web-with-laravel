94a93eacd15085d05323a76a5860d1f19aa5f171
Fuadseid <fuaddbus@gmail.com>
2025-03-24 09:29:12 +0300

    creating a signup page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home.index');
 });
+
+Route::get('/signup',[SignupController::class,'create']);
